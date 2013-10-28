ce604cd2d8750020d1dedf1478d77ef7d13f9be2
Sebastien Binet <binet@cern.ch>
2013-10-28 18:24:48 +0100

    mgr: gofmt

--- mgr.go
+++ mgr.go
@@ -2,10 +2,10 @@ package cmt
 
 // Mgr manages CMT environments
 type Mgr struct {
-	name string // project name
-	topdir string // directory holding the whole project/workarea
+	name        string // project name
+	topdir      string // directory holding the whole project/workarea
 	asetup_root string // path to asetup
-	sh Shell // subshell where CMT is configured
+	sh          Shell  // subshell where CMT is configured
 }
 
 func (mgr *Mgr) init() error {
