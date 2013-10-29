7151ed80834d8bcb9c0173839e4f1fd071f048bc
Sebastien Binet <binet@cern.ch>
2013-10-29 23:33:53 +0100

    shell: implement the reader loop

--- shell.go
+++ shell.go
@@ -1,15 +1,24 @@
 package cmt
 
 import (
+	//"bytes"
 	"fmt"
 	"io"
+	//"os"
 	"os/exec"
+	"strings"
 )
 
 type Shell struct {
 	cmd    *exec.Cmd
-	Stdin  io.Writer
-	Stdout io.Reader
+	stdin  io.Writer
+	stdout io.Reader
+	resp   chan response
+}
+
+type response struct {
+	buf []byte
+	err error
 }
 
 func NewShell() (Shell, error) {
@@ -22,10 +31,22 @@ func NewShell() (Shell, error) {
 
 	sh := Shell{
 		cmd:    cmd,
-		Stdin:  w,
-		Stdout: r,
+		stdin:  w,
+		stdout: r,
+		resp:   make(chan response),
 	}
 
+	go func() {
+		for {
+			buf := make([]byte, 1024)
+			//fmt.Fprintf(os.Stderr, "==>\n")
+			n, err := sh.stdout.Read(buf)
+			//fmt.Fprintf(os.Stderr, "==> %v (err=%v)\n", n, err)
+			buf = buf[:n]
+			//fmt.Fprintf(os.Stderr, "<<< [%v]\n", string(buf))
+			sh.resp <- response{buf, err}
+		}
+	}()
 	err := cmd.Start()
 	if err != nil {
 		return sh, err
@@ -34,16 +55,23 @@ func NewShell() (Shell, error) {
 }
 
 func (sh *Shell) Setenv(key, value string) error {
-	_, err := sh.Stdin.Write([]byte(fmt.Sprintf("export %s=%s\n", key, value)))
+	_, err := sh.stdin.Write([]byte(fmt.Sprintf("export %s=%s\n", key, value)))
 	return err
 }
 
 func (sh *Shell) Getenv(key string) string {
-	_, err := sh.Stdin.Write([]byte(fmt.Sprintf("echo ${%s}\n", key)))
+	_, err := sh.stdin.Write([]byte(fmt.Sprintf("echo ${%s}\n", key)))
 	if err != nil {
 		return ""
 	}
-	return "boo"
+	resp := <-sh.resp
+	if resp.err != nil {
+		return ""
+	}
+	out := string(resp.buf)
+	out = strings.Trim(out, "\r\n")
+	//fmt.Printf("::: %s [%v]\n", key, []byte(out))
+	return out
 }
 
 // EOF
