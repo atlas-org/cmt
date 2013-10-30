2f5df0bb369a32765c81e1be997fae5fa9ea8fe8
Sebastien Binet <binet@cern.ch>
2013-10-30 23:02:43 +0100

    cmt: first stab at a Cmt context

---
+++ cmt.go
@@ -0,0 +1,26 @@
+package cmt
+
+import (
+	"bytes"
+
+	"github.com/atlas-org/shell"
+)
+
+type Cmt struct {
+	sh shell.Shell // environment configured for cmt
+	bin string // path to cmt.exe
+}
+
+func New(sh shell.Shell) (*Cmt, error) {
+	out, err := sh.Run("which", "cmt.exe")
+	if err != nil {
+		return nil, err
+	}
+	bin := string(bytes.Trim(out, "\n"))
+	return &Cmt{
+		sh: sh,
+		bin: bin,
+	}, nil
+}
+
+// EOF
