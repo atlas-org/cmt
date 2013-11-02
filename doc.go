6b89b8e3ed46643d399d2ce3555ed01eaf956969
Sebastien Binet <binet@cern.ch>
2013-11-02 10:28:57 +0100

    doc: add some godoc doc.

---
+++ doc.go
@@ -0,0 +1,9 @@
+// cmt is a wrapper around the binary cmt.exe to expose a nicer API.
+//
+// cmt manages subshell to setup the environment suitable for cmt.exe
+// and run cmt.exe commands.
+// The output of such invocations are then formated, analyzed and massaged
+// back to the user.
+package cmt
+
+// EOF
